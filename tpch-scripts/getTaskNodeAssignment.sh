575f8c063f4a371094b9649ed0e1a219b4133906
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2016-10-18 15:38:52 -0700

    Update getTaskNodeAssignment.sh

--- tpch-scripts/getTaskNodeAssignment.sh
+++ tpch-scripts/getTaskNodeAssignment.sh
@@ -3,4 +3,4 @@ echo "query,node,count" > node_assignment_report.csv ;
 while IFS= read -r -d $'\0' file; do     
   t=$(echo $file | cut -d "/" -f2);
   zipgrep -h "nodeHttpAddress[^,]*" $file | sed 's/part.*: //g' | sed 's/nodeHttpAddress": //g'   | sed 's/"//g' | sed 's/,//g' | sort |  uniq -c | awk '{print $2 "," $1 }' | sed  "s/^/${t},/g" >> node_assignment_report.csv ;
-done < <(find . -name *.zip -type f -print0)
+done < <(find $1 -name *.zip -type f -print0)
