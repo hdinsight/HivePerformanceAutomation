2948b73ea8b1a30bf8f7e3b5445222d386490b68
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2016-10-19 19:12:17 -0700

    putting the perfkit in home dir (#13)
    * putting the perfkit in home dir
    
    * Update azuredeploy.json

--- azure/perf_installer.sh
+++ azure/perf_installer.sh
@@ -3,6 +3,7 @@
 wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
 
 if [[ `hostname -f` == `get_primary_headnode` ]]; then
+	cd $1
 	wget https://github.com/hdinsight/HivePerformanceAutomation/archive/master.zip
 	unzip master.zip;
 fi
