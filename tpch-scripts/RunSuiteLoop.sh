8e4beebce49d18c3596c7758ba0f918a7dfe93c5
kalyanr <kraman88@gmail.com>
2016-10-25 21:28:21 -0700

    Updating RunSuiteLoop2
    Creating the appropriate run directory

--- tpch-scripts/RunSuiteLoop.sh
+++ tpch-scripts/RunSuiteLoop.sh
@@ -6,6 +6,10 @@ then
 	exit 1
 fi
 
+BENCH_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd );
+echo "\$BENCH_HOME is set to $BENCH_HOME";
+BENCHMARK=hive-testbench
+
 counter=0
 while [ $counter -lt $1 ]; do
 STARTDATE="`date +%Y/%m/%d:%H:%M:%S`"
@@ -14,6 +18,8 @@ REPEAT_COUNT=$1
 let counter=counter+1
 echo "Running Iteration $counter"
 RUN_ID=$counter
+mkdir $BENCH_HOME/$BENCHMARK/run_$RUN_ID/
+
 for i in {1..22}
 do
 ./GetPatData.sh $3 ./TpchQueryExecute.sh $2 $i $RUN_ID $RUN_ID/tpch_query_$i
