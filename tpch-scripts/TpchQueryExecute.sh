d228c54668c4b576a055dc72ab320372b0ccb7e1
kalyanr <kraman88@gmail.com>
2016-11-02 17:41:20 -0700

    Bug fix for capturing query exec times (#23)
    * Bug fix for capturing query exec times
    
    * bug fix query exec 2
    
    * bug fix 3

--- tpch-scripts/TpchQueryExecute.sh
+++ tpch-scripts/TpchQueryExecute.sh
@@ -48,11 +48,18 @@ mkdir $LOG_DIR
 fi
 
 LOG_FILE_EXEC_TIMES="$LOG_DIR/query_times.csv"
+EXPLAIN_FILE_EXEC_TIMES="$PLAN_DIR/explain_times.csv"
 
 if [ ! -e "$LOG_FILE_EXEC_TIMES" ]
   then
 	touch "$LOG_FILE_EXEC_TIMES"
 	echo "QUERY,DURATION_IN_SECONDS,STARTTIME,STOPTIME,BENCHMARK,DATABASE,SCALE_FACTOR,FILE_FORMAT,STATUS" >> "${LOG_FILE_EXEC_TIMES}"
+fi
+
+if [ ! -e "$EXPLAIN_FILE_EXEC_TIMES" ]
+  then
+	touch "$EXPLAIN_FILE_EXEC_TIMES"
+	echo "QUERY,DURATION_IN_SECONDS,STARTTIME,STOPTIME,BENCHMARK,DATABASE,SCALE_FACTOR,FILE_FORMAT" >> "${EXPLAIN_FILE_EXEC_TIMES}"
 fi
 
 if [ ! -w "$LOG_FILE_EXEC_TIMES" ]
@@ -74,17 +81,18 @@ RETURN_VAL=1
 EXECUTION_COUNT=0
 STATUS=FAIL
 TIMEOUT="3h"
-#Measure time for query execution
-# Start timer to measure data loading for the file formats
-STARTDATE="`date +%Y/%m/%d:%H:%M:%S`"
-STARTTIME="`date +%s`" # seconds since epochstart
+
 # Execute query
 	ENGINE=hive
 	printf -v j "%02d" $2
 	echo "Hive query: ${2}"
 	while [ $RETURN_VAL -ne 0 -a $EXECUTION_COUNT -lt $RETRY_COUNT ]
 	do	
+		EXPLAIN_STARTTIME="`date +%s`"
 		hive -i ${HIVE_SETTING} --database ${DATABASE} -d EXPLAIN="explain formatted" -f ${QUERY_DIR}/tpch_query${2}.sql > ${PLAN_DIR}/plan_${DATABASE}_query${j}.txt 2>&1
+		EXPLAIN_ENDTIME="`date +%s`"
+		#Measure time for query execution
+		STARTTIME="`date +%s`" # seconds since epochstart
 
 		timeout ${TIMEOUT} hive -i ${HIVE_SETTING} --database ${DATABASE} -d EXPLAIN="" -f ${QUERY_DIR}/tpch_query${2}.sql > ${RESULT_DIR}/${DATABASE}_query${j}.txt 2>&1
 		RETURN_VAL=$?
@@ -98,11 +106,13 @@ STARTTIME="`date +%s`" # seconds since epochstart
 		fi
 			
 		# Calculate the time
-		STOPDATE="`date +%Y/%m/%d:%H:%M:%S`"
 		STOPTIME="`date +%s`" # seconds since epoch
+		EXPLAIN_DIFF_IN_SECONDS="$(($EXPLAIN_ENDTIME - $EXPLAIN_STARTTIME))"
 		DIFF_IN_SECONDS="$(($STOPTIME - $STARTTIME))"
 		DIFF_ms="$(($DIFF_IN_SECONDS * 1000))"
 		DURATION="$(($DIFF_IN_SECONDS / 3600 ))h $((($DIFF_IN_SECONDS % 3600) / 60))m $(($DIFF_IN_SECONDS % 60))s"
-		# log the times in load_time.csv file
+		# log the times in query_times.csv file
 		echo "query${j},${DIFF_IN_SECONDS},${STARTTIME},${STOPTIME},${BENCHMARK},${DATABASE},${SCALE},${FILE_FORMAT},${STATUS}" >> ${LOG_FILE_EXEC_TIMES}
+		# log the explain times in explain_times.csv file
+		echo "query${j},${EXPLAIN_DIFF_IN_SECONDS},${EXPLAIN_STARTTIME},${EXPLAIN_ENDTIME},${BENCHMARK},${DATABASE},${SCALE},${FILE_FORMAT}" >> ${EXPLAIN_FILE_EXEC_TIMES}
 	 done
