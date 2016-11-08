af61d3d7edf8562083c66ace2d8fc1f2c2d04d14
kraman88 <kraman88@gmail.com>
2016-11-08 14:16:12 -0800

    collect app perf data for loops 2

--- tpch-scripts/RunSingleQueryLoop.sh
+++ tpch-scripts/RunSingleQueryLoop.sh
@@ -3,7 +3,7 @@
 
 if [ $# -ne 4 ]
 then
-	echo "Usage ./RunSingleQueryLoop QUERY_NUMBER REPEAT_COUNT SCALCE_FACTOR CLUSTER_SSH_PASSWORD"
+	echo "Usage ./RunSingleQueryLoop QUERY_NUMBER REPEAT_COUNT SCALE_FACTOR CLUSTER_SSH_PASSWORD"
 	exit 1
 fi
 
@@ -15,7 +15,7 @@ let counter=counter+1
 echo "Running Iteration $counter"
 ./GetPatData.sh $4 ./TpchQueryExecute.sh $3 $1 $counter $counter/tpch_query_$(printf %02d $1) 
 
-RUN_ID=$counter
+RUN_ID=query_$1_$counter
 RESULT_DIR=$BENCH_HOME/$BENCHMARK/run_$RUN_ID/results/
 
 echo "collecting perf data"
