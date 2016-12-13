863dc0de542149e95190c3384dd84283a90da382
kraman88 <kraman88@gmail.com>
2016-12-13 10:12:05 -0800

    set up mods

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,12 +3,46 @@
 set -x
 sudo apt-get --yes install git
 
+# Check if Maven is installed and install it if not.
+which mvn > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	SKIP=0
+	if [ -e "apache-maven-3.0.5-bin.tar.gz" ]; then
+		SIZE=`du -b apache-maven-3.0.5-bin.tar.gz | cut -f 1`
+		if [ ${SIZE} -eq 5144659 ]; then
+			SKIP=1
+		fi
+	fi
+	if [ ${SKIP} -ne 1 ]; then
+		echo "Maven not found, automatically installing it."
+		curl -o ${CURRENT_DIR}/apache-maven-3.0.5-bin.tar.gz http://www.us.apache.org/dist/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz  2> /dev/null
+		if [ $? -ne 0 ]; then
+			echo "Failed to download Maven, check Internet connectivity and try again."
+			exit 1
+		fi
+	fi
+	tar -zxf ${CURRENT_DIR}/apache-maven-3.0.5-bin.tar.gz -C ${CURRENT_DIR} > /dev/null
+fi
+
 which sshpass &> /dev/null || sudo apt-get -y -qq install sshpass
 which pdsh &> /dev/null || sudo apt-get -y -qq install pdsh
 
 if [ ! -d "${OUTPUT_PATH}/azlogs" ]; then
 	git clone https://github.com/dharmeshkakadia/azlogs ${OUTPUT_PATH} ;
-    {CURRENT_DIRECTORY}/apache-maven-3.0.5/bin/mvn package assembly:single -f ${OUTPUT_PATH}/azlogs/pom.xml;
+    {CURRENT_DIR}/apache-maven-3.0.5/bin/mvn package assembly:single -f ${OUTPUT_PATH}/azlogs/pom.xml;
+fi
+
+which csvsql > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Installing CSVKIT" 
+	# 3.5 cluster ships with older version of pip which fails the package install
+	pip install --upgrade pip
+	sudo pip install csvkit
+fi
+
+if [ ! -d "${OUTPUT_PATH}/azlogs" ]; then
+	git clone https://github.com/dharmeshkakadia/azlogs ${OUTPUT_PATH} ;
+    {CURRENT_DIR}/apache-maven-3.0.5/bin/mvn package assembly:single -f ${OUTPUT_PATH}/azlogs/pom.xml;
 fi
 
 which csvsql > /dev/null 2>&1
