8956e36c6d084fdd09c723b87fff685cbb0b2531
kraman88 <kraman88@gmail.com>
2016-12-19 09:07:01 -0800

    Remove generate plans

--- bin/queryExecutor.sh
+++ bin/queryExecutor.sh
@@ -18,30 +18,6 @@ name=${FILE##*/}
 basename=${name%.sql}
 basename=${basename%.txt}
 
-if ${GENERATE_PLANS};
-then
-
-    querytext=$(cat $FILE | tac | sed '0,/select/s/select/explain select/I' | tac)
-    PLANSTARTTIME="`date +%s`"
-    if ! [[ $RUN_ID =~  ^[0-9]+$ ]]
-    then
-        FILENAME_EXTENSION=_${PLANSTARTTIME}
-    fi
-    beeline -u ${CONNECTION_STRING} -i ${HIVE_SETTING} --hivevar DB=${DATABASE} -e "$querytext" > ${PLANS_DIR}/plan_${DATABASE}_${basename}${FILENAME_EXTENSION}.json 2 >> ${PLANS_DIR}/plan_debug.log
-    RETURN_VAL=$?
-    PLANENDTIME="`date +%s`"
-
-    if [ $RETURN_VAL = 0 ]
-    then
-        STATUS=SUCCESS
-    else
-        STATUS=FAIL
-    fi
-
-    DIFF_IN_SECONDS="$(($PLANENDTIME - $PLANSTARTTIME))"
-    echo "$basename,${DIFF_IN_SECONDS},${PLANSTARTTIME},${PLANENDTIME},${WORKLOAD},${QUERY_DATABASE},${STATUS}" >> ${PLAN_TIMES_FILE}
-fi
-
 QUERYSTARTTIME="`date +%s`"
 beeline -u ${CONNECTION_STRING} -i ${HIVE_SETTING} --hivevar DB=${DATABASE} -f $FILE > ${RESULTS_DIR}/${DATABASE}_${basename}${FILENAME_EXTENSION}.txt 2>&1
 RETURN_VAL=$?
