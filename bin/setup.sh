79ed60597bc600c626fe53ceb9fa25e7aa98c529
kalyanr <kraman88@gmail.com>
2017-01-22 23:47:59 -0800

    remove dups in setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -27,11 +27,6 @@ fi
 which sshpass &> /dev/null || sudo apt-get -y -qq install sshpass
 which pdsh &> /dev/null || sudo apt-get -y -qq install pdsh
 
-if [ ! -d "${OUTPUT_PATH}/azlogs" ]; then
-	git clone https://github.com/dharmeshkakadia/azlogs ${OUTPUT_PATH} ;
-    {CURRENT_DIR}/apache-maven-3.0.5/bin/mvn package assembly:single -f ${OUTPUT_PATH}/azlogs/pom.xml;
-fi
-
 which csvsql > /dev/null 2>&1
 if [ $? -ne 0 ]; then
 	echo "Installing CSVKIT" 
@@ -41,23 +36,9 @@ if [ $? -ne 0 ]; then
 fi
 
 if [ ! -d "${OUTPUT_PATH}/azlogs" ]; then
-	git clone https://github.com/dharmeshkakadia/azlogs ${OUTPUT_PATH} ;
+	git clone https://github.com/dharmeshkakadia/azlogs ${OUTPUT_PATH}/azlogs ;
     {CURRENT_DIR}/apache-maven-3.0.5/bin/mvn package assembly:single -f ${OUTPUT_PATH}/azlogs/pom.xml;
 fi
-
-which csvsql > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-	echo "Installing CSVKIT" 
-	# 3.5 cluster ships with older version of pip which fails the package install
-	pip install --upgrade pip
-	sudo pip install csvkit
-fi
-
-for slave in `cat /etc/hadoop/conf/slaves`
-do
-        #echo "ssh-copy-id on $slave"
-        sshpass -p $1 ssh-copy-id -o StrictHostKeyChecking=no ${slave}
-done
 
 pdsh -R ssh -w ^/etc/hadoop/conf/slaves sudo apt-get -y -qq install linux-tools-common sysstat gawk
 
