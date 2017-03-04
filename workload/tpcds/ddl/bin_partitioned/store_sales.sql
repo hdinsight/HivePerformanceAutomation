eb8d8cb2f3296e2ee2f8cadd1666792f56c4821b
Dharmesh Kakadia <dharmesh.kakadia@gmail.com>
2017-03-03 16:22:46 -0800

    add the files in new location

---
+++ workload/tpcds/ddl/bin_partitioned/store_sales.sql
@@ -0,0 +1,59 @@
+create database if not exists ${DB};
+use ${DB};
+
+drop table if exists store_sales;
+
+create table store_sales
+(
+    ss_sold_time_sk           int,
+    ss_item_sk                int,
+    ss_customer_sk            int,
+    ss_cdemo_sk               int,
+    ss_hdemo_sk               int,
+    ss_addr_sk                int,
+    ss_store_sk               int,
+    ss_promo_sk               int,
+    ss_ticket_number          bigint,
+    ss_quantity               int,
+    ss_wholesale_cost         float,
+    ss_list_price             float,
+    ss_sales_price            float,
+    ss_ext_discount_amt       float,
+    ss_ext_sales_price        float,
+    ss_ext_wholesale_cost     float,
+    ss_ext_list_price         float,
+    ss_ext_tax                float,
+    ss_coupon_amt             float,
+    ss_net_paid               float,
+    ss_net_paid_inc_tax       float,
+    ss_net_profit             float
+)
+partitioned by (ss_sold_date_sk int)
+stored as ${FILE};
+
+insert overwrite table store_sales partition (ss_sold_date_sk) 
+select
+        ss.ss_sold_time_sk,
+        ss.ss_item_sk,
+        ss.ss_customer_sk,
+        ss.ss_cdemo_sk,
+        ss.ss_hdemo_sk,
+        ss.ss_addr_sk,
+        ss.ss_store_sk,
+        ss.ss_promo_sk,
+        ss.ss_ticket_number,
+        ss.ss_quantity,
+        ss.ss_wholesale_cost,
+        ss.ss_list_price,
+        ss.ss_sales_price,
+        ss.ss_ext_discount_amt,
+        ss.ss_ext_sales_price,
+        ss.ss_ext_wholesale_cost,
+        ss.ss_ext_list_price,
+        ss.ss_ext_tax,
+        ss.ss_coupon_amt,
+        ss.ss_net_paid,
+        ss.ss_net_paid_inc_tax,
+        ss.ss_net_profit,
+        ss.ss_sold_date_sk
+      from ${SOURCE}.store_sales ss;
---
+++ workload/tpcds/tpcds-setup.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+function usage {
+	echo "Usage: tpcds-setup.sh scale_factor [temp_directory]"
+	exit 1
+}
+
+function runcommand {
+	if [ "X$DEBUG_SCRIPT" != "X" ]; then
+		$1
+	else
+		$1 2>/dev/null
+	fi
+}
+
+if [ ! -f tpcds-gen/target/tpcds-gen-1.0-SNAPSHOT.jar ]; then
+	echo "Please build the data generator with ./tpcds-build.sh first"
+	exit 1
+fi
+which hive > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Script must be run where Hive is installed"
+	exit 1
+fi
+
+# Tables in the TPC-DS schema.
+DIMS="date_dim time_dim item customer customer_demographics household_demographics customer_address store promotion warehouse ship_mode reason income_band call_center web_page catalog_page web_site"
+FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_returns inventory"
+
+# Get the parameters.
+SCALE=$1
+DIR=$2
+if [ "X$BUCKET_DATA" != "X" ]; then
+	BUCKETS=13
+	RETURN_BUCKETS=13
+else
+	BUCKETS=1
+	RETURN_BUCKETS=1
+fi
+if [ "X$DEBUG_SCRIPT" != "X" ]; then
+	set -x
+fi
+
+# Sanity checking.
+if [ X"$SCALE" = "X" ]; then
+	usage
+fi
+if [ X"$DIR" = "X" ]; then
+	DIR=/tmp/tpcds-generate
+fi
+if [ $SCALE -eq 1 ]; then
+	echo "Scale factor must be greater than 1"
+	exit 1
+fi
+
+# Do the actual data load.
+hdfs dfs -mkdir -p ${DIR}
+hdfs dfs -ls ${DIR}/${SCALE} > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Generating data at scale factor $SCALE."
+	(cd tpcds-gen; hadoop jar target/*.jar -d ${DIR}/${SCALE}/ -s ${SCALE})
+fi
+hdfs dfs -ls ${DIR}/${SCALE} > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Data generation failed, exiting."
+	exit 1
+fi
+echo "TPC-DS text data generation complete."
+
+# Create the text/flat tables as external tables. These will be later be converted to ORCFile.
+echo "Loading text data into external tables."
+runcommand "hive -i settings/load-flat.sql -f ddl-tpcds/text/alltables.sql -d DB=tpcds_text_${SCALE} -d LOCATION=${DIR}/${SCALE}"
+
+# Create the partitioned and bucketed tables.
+if [ "X$FORMAT" = "X" ]; then
+	FORMAT=orc
+fi
+i=1
+total=24
+DATABASE=tpcds_bin_partitioned_${FORMAT}_${SCALE}
+for t in ${FACTS}
+do
+	echo "Optimizing table $t ($i/$total)."
+	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
+            -d SCALE=${SCALE} \
+	    -d SOURCE=tpcds_text_${SCALE} -d BUCKETS=${BUCKETS} \
+	    -d RETURN_BUCKETS=${RETURN_BUCKETS} -d FILE=${FORMAT}"
+	runcommand "$COMMAND"
+	if [ $? -ne 0 ]; then
+		echo "Command failed, try 'export DEBUG_SCRIPT=ON' and re-running"
+		exit 1
+	fi
+	i=`expr $i + 1`
+done
+
+# Populate the smaller tables.
+for t in ${DIMS}
+do
+	echo "Optimizing table $t ($i/$total)."
+	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} -d SOURCE=tpcds_text_${SCALE} \
+            -d SCALE=${SCALE} \
+	    -d FILE=${FORMAT}"
+	runcommand "$COMMAND"
+	if [ $? -ne 0 ]; then
+		echo "Command failed, try 'export DEBUG_SCRIPT=ON' and re-running"
+		exit 1
+	fi
+	i=`expr $i + 1`
+done
+
+echo "Data loaded into database ${DATABASE}."
